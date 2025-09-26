6e46bee59e4c21f64f644762396735f2be44cf15
Alin-Florin Ciu <alinflorinciu@icloud.com>
2025-09-26 09:17:24 +0300

    -

--- src/stdio-boot.ts
+++ src/stdio-boot.ts
@@ -5,10 +5,4 @@ dotenv.config({
 import createServer from "./server";
 
 const server = createServer();
-
-server.start({
-  transportType: "httpStream",
-  httpStream: {
-    port: 8080,
-  },
-});
+server.start({transportType: "stdio"});
