7af383c021bd71e5c831446947831eb1a2b55578
Alin-Florin Ciu <alinflorinciu@icloud.com>
2025-09-27 11:10:42 +0300

    as

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,48 @@
 import dotenv from "dotenv";
 dotenv.config({
-    quiet: true
+  quiet: true,
 });
-import createServer from "./server";
+import { FastMCP } from "fastmcp";
+import { VERSION } from "./version";
+import { z } from "zod";
 
-const server = createServer();
+const server = new FastMCP({
+  name: "CalDAV MCP",
+  version: VERSION,
+  authenticate: async (request) => {
+    if (!process.env.API_KEY || !request) {
+      return { id: "user" };
+    }
+    const apiKey = request.headers["authorization"]?.replace("Bearer ", "");
+    if (apiKey !== process.env.API_KEY) {
+      throw new Response(null, {
+        status: 401,
+        statusText: "Unauthorized",
+      });
+    }
+    return {
+      id: "user",
+    };
+  },
+});
 
-let transport: 'httpStream' | 'stdio' = 'stdio';
-if (process.env.TRANSPORT === 'http') {
-    transport = 'httpStream';
+server.addTool({
+  description: "A simple tool that returns a greeting message.",
+  name: "hello_world",
+  parameters: z.object({
+    name: z.string(),
+  }),
+  execute: async (args, ctx) => {
+    return "";
+  },
+});
+
+let transport: "httpStream" | "stdio" = "stdio";
+if (process.env.TRANSPORT === "http") {
+  transport = "httpStream";
 }
-if (process.argv[2] === '--http') {
-    transport = 'httpStream';
+if (process.argv[2] === "--http") {
+  transport = "httpStream";
 }
 server.start({
   transportType: transport,
